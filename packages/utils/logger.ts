d7d64e85cad724beddfef2c044e00f0158372bfc
Max Moon <max@maxmoon.sh>
2025-02-17 20:10:51 +0000

    feat: add logger

---
+++ packages/utils/logger.ts
@@ -0,0 +1,53 @@
+import winston from "winston";
+import chalk from "chalk";
+
+const debugEnabled = process.argv.some(
+  (arg) => arg === "-D" || arg === "--debug",
+);
+
+const levelColors: { [key: string]: (msg: string) => string } = {
+  error: chalk.red,
+  warn: chalk.yellow,
+  info: chalk.green,
+  debug: chalk.blue,
+};
+
+const customFormat = winston.format.printf(({ level, message, timestamp }) => {
+  const colorizer = levelColors[level] || ((msg: string) => msg);
+  return `${chalk.gray(timestamp)} [${colorizer(level.toUpperCase())}]: ${message}`;
+});
+
+const logger = winston.createLogger({
+  level: debugEnabled ? "debug" : "info",
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    customFormat,
+  ),
+  transports: [new winston.transports.Console()],
+});
+
+export class Logger {
+  static debug(message: string) {
+    logger.debug(message);
+  }
+
+  static info(message: string) {
+    logger.info(message);
+  }
+
+  static warn(message: string) {
+    logger.warn(message);
+  }
+
+  static error(message: string | object, err?: unknown) {
+    if (typeof message === "object") {
+      logger.error(JSON.stringify(message, null, 2));
+    } else {
+      let msg = message;
+      if (err) {
+        msg += "\n" + JSON.stringify(err, null, 2);
+      }
+      logger.error(msg);
+    }
+  }
+}

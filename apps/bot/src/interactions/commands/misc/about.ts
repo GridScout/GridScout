134ab2608a198ccdc54d5a5e90b4c62460bd3990
Max Moon <max@maxmoon.sh>
2025-03-06 21:06:28 +0000

    feat: about command

---
+++ apps/bot/src/interactions/commands/misc/about.ts
@@ -0,0 +1,49 @@
+import i18next from "@gridscout/lang";
+
+import SlashCommand from "../../../structures/slashCommand.js";
+import { client } from "../../../index.js";
+
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  MessageFlags,
+  SlashCommandBuilder,
+  type ChatInputCommandInteraction,
+} from "discord.js";
+
+export default class Command extends SlashCommand {
+  constructor() {
+    super("about", "Info about GridScout");
+  }
+
+  override async execute(
+    interaction: ChatInputCommandInteraction,
+    locale: string
+  ) {
+    await interaction.deferReply({
+      flags: MessageFlags.Ephemeral,
+    });
+
+    const pingButton = new ButtonBuilder()
+      .setCustomId("ping")
+      .setLabel(`${client.ws.ping}ms`)
+      .setStyle(ButtonStyle.Secondary)
+      .setEmoji("1342549911715053629")
+      .setDisabled(true);
+
+    await interaction.editReply({
+      content: i18next.t("about.content", { lng: locale }),
+      components: [
+        new ActionRowBuilder<ButtonBuilder>().addComponents(pingButton),
+      ],
+    });
+  }
+
+  override async build() {
+    return new SlashCommandBuilder()
+      .setName(this.name)
+      .setDescription(this.description)
+      .toJSON();
+  }
+}

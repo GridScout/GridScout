cc922a17c693dae4514d5e282668516e445f5df0
Max Moon <max@maxmoon.sh>
2025-03-06 21:05:13 +0000

    feat: lang package with locales & emojis

---
+++ packages/lang/src/index.ts
@@ -0,0 +1,28 @@
+import logger from "@gridscout/logger";
+
+import i18next from "i18next";
+import Backend from "i18next-fs-backend";
+
+import path from "node:path";
+
+i18next.use(Backend).init(
+  {
+    backend: {
+      loadPath: path.join(__dirname, "./locales/{{lng}}.json"),
+    },
+    fallbackLng: "en",
+    preload: ["en"],
+    interpolation: {
+      escapeValue: false,
+    },
+  },
+  (err) => {
+    if (err) {
+      logger.error("Error loading i18next:", err);
+    } else {
+      logger.debug("i18next loaded successfully");
+    }
+  }
+);
+
+export default i18next;

8ff112593479379db4b16c81890fa957a6f40c9e
Max Moon <max@maxmoon.sh>
2025-03-17 14:41:33 +0000

    refactor: streamline translation handling and improve driver data formatting

--- apps/bot/src/interactions/commands/driver/driver.ts
+++ apps/bot/src/interactions/commands/driver/driver.ts
@@ -26,6 +26,8 @@ export default class Command extends SlashCommand {
     locale: string
   ) {
     await interaction.deferReply();
+    
+    const t = (key: string, options = {}) => i18next.t(key, { lng: locale, ...options });
 
     // Get the driver name from the interaction options
     let driver = interaction.options.getString("driver");
@@ -33,13 +35,14 @@ export default class Command extends SlashCommand {
     // If no driver empty, return an error
     if (!driver) {
       return interaction.editReply({
-        embeds: [errorEmbed("", i18next.t("genericError", { lng: locale }))],
+        embeds: [errorEmbed("", t("genericError"))],
       });
     }
 
-    // Search for the driver in Meilisearch
-    const driverSearch = await meilisearch.searchDriverByName(driver);
-    if (driverSearch.length > 0) driver = driverSearch[0]?.id ?? driver;
+    if (!/^[a-z0-9]+$/.test(driver)) {
+      const driverSearch = await meilisearch.searchDriverByName(driver);
+      if (driverSearch.length > 0) driver = driverSearch[0]?.id ?? driver;
+    }
 
     // Get the driver data from the API
     const driverData = await api.driver.get(driver);
@@ -47,7 +50,7 @@ export default class Command extends SlashCommand {
     // If the driver data is an error, return an error embed
     if (driverData.isErr()) {
       return interaction.editReply({
-        embeds: [errorEmbed("", i18next.t("driver.error", { lng: locale }))],
+        embeds: [errorEmbed("", t("driver.error"))],
       });
     }
 
@@ -69,29 +72,29 @@ export default class Command extends SlashCommand {
     const timetag = `<t:${new Date(driverInfo.dateOfBirth).getTime() / 1000}:R>`;
 
     embed.setDescription(
-      `${i18next.t("driver.acronym", { lng: locale, acronym: driverInfo.abbreviation })}\n` +
-        `${i18next.t("driver.constructor", { lng: locale, constructorEmoji, name: driverInfo.team.name })}\n` +
-        `${i18next.t("driver.dob", { lng: locale, dob: driverInfo.dateOfBirth, timetag })}\n` +
-        `${i18next.t("driver.nationality", { lng: locale, flag: countryEmojis[driverInfo.nationality.alpha3 as keyof typeof countryEmojis], nationality: `${driverInfo.nationality.demonym}` })}\n`
+      `${t("driver.acronym", { acronym: driverInfo.abbreviation })}\n` +
+        `${t("driver.constructor", { constructorEmoji, name: driverInfo.team.name })}\n` +
+        `${t("driver.dob", { dob: formatDate(driverInfo.dateOfBirth, locale, t), timetag })}\n` +
+        `${t("driver.nationality", { flag: countryEmojis[driverInfo.nationality.alpha3 as keyof typeof countryEmojis], nationality: `${driverInfo.nationality.demonym}` })}\n`
     );
 
     embed.addFields([
       {
-        name: i18next.t("driver.statistics", { lng: locale }),
+        name: t("driver.statistics"),
         value:
-          `${i18next.t("driver.wdc", { lng: locale, amount: driverInfo.statistics.worldChampionships })}\n` +
-          `${i18next.t("driver.highestRaceFinish", { lng: locale, position: driverInfo.statistics.highestRaceFinish || 0 })}\n` +
-          `${i18next.t("driver.highestGridPosition", { lng: locale, position: driverInfo.statistics.highestGridPosition || 0 })}\n` +
-          `${i18next.t("driver.racesEntered", { lng: locale, amount: driverInfo.statistics.racesEntered })}\n`,
+          `${t("driver.wdc", { amount: driverInfo.statistics.worldChampionships })}\n` +
+          `${t("driver.highestRaceFinish", { position: driverInfo.statistics.highestRaceFinish || 0 })}\n` +
+          `${t("driver.highestGridPosition", { position: driverInfo.statistics.highestGridPosition || 0 })}\n` +
+          `${t("driver.racesEntered", { amount: driverInfo.statistics.racesEntered })}\n`,
         inline: true,
       },
       {
         name: "_ _",
         value:
-          `${i18next.t("driver.podiums", { lng: locale, amount: driverInfo.statistics.podiums })}\n` +
-          `${i18next.t("driver.points", { lng: locale, amount: driverInfo.statistics.points })}\n` +
-          `${i18next.t("driver.fastestLaps", { lng: locale, amount: driverInfo.statistics.fastestLaps })}\n` +
-          `${i18next.t("driver.grandslams", { lng: locale, amount: driverInfo.statistics.grandSlams })}\n`,
+          `${t("driver.podiums", { amount: driverInfo.statistics.podiums })}\n` +
+          `${t("driver.points", { amount: driverInfo.statistics.points })}\n` +
+          `${t("driver.fastestLaps", { amount: driverInfo.statistics.fastestLaps })}\n` +
+          `${t("driver.grandslams", { amount: driverInfo.statistics.grandSlams })}\n`,
         inline: true,
       },
     ]);
@@ -99,31 +102,15 @@ export default class Command extends SlashCommand {
     // Set the thumbnail to the driver image from wikipedia
     embed.setThumbnail(driverInfo.image);
 
-    // Generated the ANSI-styled recent races code block
-    const generateLastRacesANSI = (races: Driver["recentRaces"]): string => {
-      return races
-        .map((race) => {
-          const position = `${i18next.t("driver.position", { lng: locale, pos: getOrdinalSuffix(race.position, locale) })} ${race.raceGap ? `(${race.raceGap})` : ""}`;
-          const raceTime =
-            race.raceTime == null
-              ? i18next.t("driver.notAvailable", { lng: locale })
-              : race.raceTime;
-
-          return `\u001b[2;34m\u001b[1;34m${race.name}\u001b[0m\u001b[2;34m\u001b[0m \u001b[2;33m${race.date}\u001b[0m\n \u001b[2;42m\u001b[0m\u001b[2;30m├\u001b[0m ⏰ \u001b[2;36m${raceTime}\u001b[0m\n \u001b[2;30m└\u001b[0m 🏁 \u001b[2;36m${position}\u001b[0m`;
-        })
-        .join("\n\n");
-    };
-
     const lastRaces = driverInfo.recentRaces;
 
     if (lastRaces.length) {
       embed.addFields([
         {
-          name: i18next.t("driver.lastRaces", {
-            lng: locale,
+          name: t("driver.lastRaces", {
             count: lastRaces.length,
           }),
-          value: `\`\`\`ansi\n${generateLastRacesANSI(lastRaces)}\`\`\``,
+          value: `\`\`\`ansi\n${generateLastRacesANSI(lastRaces, locale, t)}\n\`\`\``,
         },
       ]);
     }
@@ -159,21 +146,48 @@ export default class Command extends SlashCommand {
   }
 }
 
-function getOrdinalSuffix(n: string, locale: string) {
-  if (isNaN(parseInt(n))) {
+function generateLastRacesANSI(races: Driver["recentRaces"], locale: string, t: (key: string, options?: object) => string): string {
+  return races
+    .map((race) => {
+      const position = isNaN(parseInt(race.position))
+        ? `${t("driver.position", { pos: getOrdinalSuffix(race.position, locale, t) })} ${race.raceGap ? `(${race.raceGap})` : ""}`
+        : `${t("driver.position", { pos: `${t("driver.finished")} ${getOrdinalSuffix(race.position, locale, t)}` })} ${race.raceGap ? `(${race.raceGap})` : ""}`;
+      const raceTime =
+        race.raceTime == null
+          ? t("driver.notAvailable")
+          : race.raceTime;
+
+      return `\u001b[2;34m\u001b[1;34m${race.name}\u001b[0m\u001b[2;34m\u001b[0m \u001b[2;33m${race.date}\u001b[0m\n \u001b[2;42m\u001b[0m\u001b[2;30m├\u001b[0m ⏰ \u001b[2;36m${raceTime}\u001b[0m\n \u001b[2;30m└\u001b[0m 🏁 \u001b[2;36m${position}\u001b[0m`;
+    })
+    .join("\n\n");
+}
+
+function getOrdinalSuffix(n: string, locale: string, t: (key: string, options?: object) => string): string {
+  const num = parseInt(n);
+  if (isNaN(num)) {
     return n;
   }
-
   const s = [
-      i18next.t("ordinal.rules.default", { lng: locale }),
-      i18next.t("ordinal.rules.1", { lng: locale }),
-      i18next.t("ordinal.rules.2", { lng: locale }),
-      i18next.t("ordinal.rules.3", { lng: locale }),
-    ],
-    v = parseInt(n) % 100;
+    t("ordinal.rules.default"),
+    t("ordinal.rules.1"),
+    t("ordinal.rules.2"),
+    t("ordinal.rules.3")
+  ];
+  const v = num % 100;
   const index = (v - 20) % 10;
-  const suffix =
-    s[index] !== undefined ? s[index] : s[v] !== undefined ? s[v] : s[0];
-  if (!suffix) return n;
-  return `${i18next.t("driver.finished", { lng: locale })} ${n}${suffix}`;
+  const suffix = s[index] !== undefined ? s[index] : s[v] !== undefined ? s[v] : s[0];
+  return `${n}${suffix}`;
+}
+
+function formatDate(date: string, locale: string, t: (key: string, options?: object) => string): string {
+  const dateObj = new Date(date);
+  const day = dateObj.getDate();
+  const ordinalDay = getOrdinalSuffix(day.toString(), locale, t);
+  return dateObj
+    .toLocaleDateString(locale, {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    })
+    .replace(day.toString(), ordinalDay);
 }
